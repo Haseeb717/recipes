4e8c65301ad6e34ede58c47f789d2f40bd583020
haseeb <haseeb@yallamotor.com>
2020-09-08 02:22:48 +0500

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,20 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   namespace :api, defaults: {format: 'json'} do
+  	namespace :v1 do
       resources :users do
         collection do
           post 'signin'
         end
       end
 
-      resources :users do
+      resources :recipes do
         collection do
           get 'five_minutes_recipes'
           get 'max_15_min_recipe'
         end
       end
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
