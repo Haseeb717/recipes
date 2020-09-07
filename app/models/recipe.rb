210d1eb48e031d14a77654ee979f6b08e4824ca5
haseeb <haseeb@yallamotor.com>
2020-09-08 01:06:36 +0500

    Changes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
-	has_many :ingredients, :through => :recipe_ingredients  # Edit :needs to be plural same as the has_many relationship   
-  has_many :recipe_ingredients
+	has_many :ingredients, :through => :recipie_ingredients  # Edit :needs to be plural same as the has_many relationship   
+  has_many :recipie_ingredients
 
   has_many :meals, :through => :recipe_meals  # Edit :needs to be plural same as the has_many relationship   
   has_many :recipe_meals
