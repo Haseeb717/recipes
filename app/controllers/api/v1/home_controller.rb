82e1a578bdf3368aed9350ca8eaa5105dbe42845
HaseebTechpath <haseeb@teckpath.com>
2020-09-12 21:41:25 +0500

    Changes

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::HomeController < Api::V1::BaseController
 	def cuisines
 		cuisines = Cuisine.all
-		render :json=>{cuisines: cuisines.to_json}
+		render :json=>{cuisines: cuisines}
 	end
 
 	def meal_types
 		meal_types = MealType.all
-		render :json=>{meal_types: meal_types.to_json}
+		render :json=>{meal_types: meal_types}
 	end
 
 
--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RecipesController < Api::V1::BaseController
 
 	def index
 		recipes = Recipe.all
-		render :json=>{recipe: recipes.to_json}
+		render :json=>{recipe: recipes}
 	end
 
 	def show
@@ -12,11 +12,11 @@ class Api::V1::RecipesController < Api::V1::BaseController
 
 	def five_minutes_recipes
 		recipes = Recipe.where(:readyInMinutes => 5)
-		render :json=>{recipe: recipes.to_json}
+		render :json=>{recipe: recipes}
 	end
 
 	def max_15_min_recipe
 		recipes = Recipe.joins(:ingredients).group('recipes.id').having('COUNT(ingredients.id) < 5').all
-		render :json=>{recipe: recipes.to_json}
+		render :json=>{recipe: recipes}
 	end
 end
